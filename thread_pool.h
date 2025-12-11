7aad896b6eec3c865a49d6cd84f380280208a4c5
Corey Lewis <corey.lewis1@morehouse.edu>
2025-12-10 20:44:02 -0500

    Update thread_pool.h

--- thread_pool.h
+++ thread_pool.h
@@ -8,7 +8,12 @@
 #include <condition_variable>
 #include <functional>
 #include <atomic>
+#include <stdexcept>
 
+/**
+ * Thread pool implementation for handling concurrent client connections
+ * Uses a fixed number of worker threads to process tasks from a queue
+ */
 class ThreadPool {
 private:
     std::vector<std::thread> workers;
@@ -20,17 +25,28 @@ private:
     std::atomic<int> active_count;
     
     int pool_size;
+    
+    void worker_thread();
 
 public:
-    ThreadPool(int size);
+    explicit ThreadPool(int size);
     ~ThreadPool();
     
+    // Delete copy constructor and assignment operator
+    ThreadPool(const ThreadPool&) = delete;
+    ThreadPool& operator=(const ThreadPool&) = delete;
+    
+    // Enqueue a task to be executed by the thread pool
     void enqueue(std::function<void()> task);
+    
+    // Get the number of currently active worker threads
     int get_active_count() const;
+    
+    // Get the total size of the thread pool
     int get_pool_size() const;
     
-private:
-    void worker_thread();
+    // Get the number of pending tasks in the queue
+    size_t get_queue_size() const;
 };
 
-#endif // THREAD_POOL_H
+#endif
